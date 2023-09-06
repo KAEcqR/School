33c9c039902d081e6c38883b581f1135b978dc7a
Kacper <kacperprochwicz@op.pl>
2023-09-06 19:01:39 +0200

    Add files via upload

---
+++ 2023,2024/Programowanie zaawansowanych aplikacji webowych/cw1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("┎ -------------------------------------┒");
+Console.WriteLine("| Kacper Prochwicz                     |");
+Console.WriteLine("| 2TIP 2023                            |");
+Console.WriteLine("┖ -------------------------------------┚");
+
+int a = 12;
+int b = 9;
+
+Console.WriteLine("a - b =  ");
+Console.WriteLine(a+b);
