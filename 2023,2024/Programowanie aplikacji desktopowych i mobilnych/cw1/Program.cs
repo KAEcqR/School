0731189239dc4ce90dd2488e6d0c9096f0eaae09
Kacper <kacperprochwicz@op.pl>
2023-09-09 15:49:58 +0200

    Add files via upload

---
+++ 2023,2024/Programowanie aplikacji desktopowych i mobilnych/cw1/Program.cs
@@ -0,0 +1,13 @@
+﻿string imie;
+Console.WriteLine("Podaj imie: ");
+imie = Console.ReadLine();
+
+string nazwisko;
+Console.WriteLine("Podaj nazwisko: ");
+nazwisko = Console.ReadLine();
+
+int age = 0;
+Console.Write("podaj wiek: ");
+age = Convert.ToInt32( Console.ReadLine());
+
+Console.WriteLine("Witaj " + imie + " Twoj wiek to " + age);
