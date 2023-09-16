ba9a28f10c038289b582d1a9b2a34957ce3b1f0b
Kacper <kacperprochwicz@op.pl>
2023-09-16 16:10:56 +0200

    Add files via upload

---
+++ 2023,2024/Programowanie aplikacji desktopowych i mobilnych/cw3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Podaj tekst");
+string tekst = Console.ReadLine();
+
+Console.WriteLine("Ile razy");
+int numer = int.Parse(Console.ReadLine());
+
+
+if(numer < 100){
+    Console.WriteLine("for");
+    for(int i = 0; i < numer; i++){
+        Console.WriteLine(tekst);
+    };
+
+    int x = 0;
+
+    Console.WriteLine("while");
+    while(numer > x){
+        Console.WriteLine(tekst);
+        x++;
+    };
+
+    x = 0;
+
+    Console.WriteLine("do while");
+    do{
+        Console.WriteLine(tekst);
+        x++;
+    }while(numer > x);
+}else{
+    Console.WriteLine("Liczba nie może przekraczać 100");
+}
---
+++ 2023,2024/Programowanie aplikacji internetowych/Kacper_P/cw1/cw1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>test</h1>
+    <?php
+        echo "Witaj";
+        echo "<center style='border: 3 vgnlllllllllllllllpx solid black'><h2>Kacper prochwicz</h2>";
+        echo "<h3>Uczen klasy 2 tip</h3>";
+        echo "<h4>Rok szkolny 2024</h4></center>";
+    ?>
+</body>
+</html>
