5e591bb0609289a16fff1b588527149918fcf094
Kacper <kacperprochwicz@op.pl>
2023-09-19 10:59:01 +0200

    Add files via upload

---
+++ 2023,2024/Programowanie aplikacji desktopowych i mobilnych/cw4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Podaj rozmiar tablicy");
+try{
+    int size = Int32.Parse(Console.ReadLine());
+    int[] numbers = new int[size];
+    Fillarray(numbers);
+    ShowArray(numbers);
+}catch(FormatException ex){
+    Console.WriteLine("Error");
+    Console.WriteLine(ex.Message);
+}
+
+void Fillarray(int[] nums){
+    Random rnd = new Random();
+    for(int i=0; i<nums.Length; i++){
+        nums[i] = rnd.Next(0,100);
+    }
+}
+
+void ShowArray(int[] nums){
+    foreach(int elem in nums){
+        Console.WriteLine($"element tablicy : {elem} ");
+    }    
+}
